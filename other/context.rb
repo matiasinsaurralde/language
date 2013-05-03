c9100cfe1f88a2769f54fb78fbe7fd426c3c4267
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2013-05-03 17:42:19 -0400

    term freq.

--- other/context.rb
+++ other/context.rb
@@ -1,4 +1,44 @@
-#!/usr/bin/env ruby
 # encoding: utf-8
 
-# context of given text
+require 'pp'
+
+class Text
+
+	def initialize(s)
+
+		@terms, @body = {}, s.downcase.gsub("\n", "")
+
+		extract_terms()
+
+	end
+
+	def extract_terms()
+
+		_terms = {}
+
+		splits = @body.split(' ')
+
+		splits.each do |t|
+			t.gsub!(/(,|\.)/, '')
+			if !_terms[t]
+				_terms.store(t, 0)
+			end
+			_terms[t] += 1
+		end
+
+		_terms.each do |term, count|
+			_terms[term] = ( count.to_f / splits.size.to_f * 100.0 ).round(1)
+		end
+
+		_terms.sort_by {|term, weight| weight }.reverse.each do |term, weight|
+			@terms.store( term, weight )
+		end
+
+	end
+
+	def terms
+		return @terms
+	end
+
+end
+
